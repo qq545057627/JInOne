1f4163142107af0cd59f93e0735534f84081e69e
yutao <qq545057627@gmail.com>
2017-07-21 10:36:31 +0800

    导入项目源码
    导入项目源码

---
+++ JInOne/app/src/main/java/com/w/jinone/MainVu.java
@@ -0,0 +1,13 @@
+package com.w.jinone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainVu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_vu);
+    }
+}
